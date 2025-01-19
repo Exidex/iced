70ea7494e5662231ff9427833d45b334468895a7
Exidex <16986685+exidex@users.noreply.github.com>
2025-01-19 18:12:40 +0100

    Expose macOS ActivationPolicy and activate_ignoring_other_apps

--- winit/src/settings.rs
+++ winit/src/settings.rs
@@ -14,6 +14,51 @@ pub struct Settings {
 
     /// The fonts to load on boot.
     pub fonts: Vec<Cow<'static, [u8]>>,
+
+    /// Platform specific settings.
+    pub platform_specific: PlatformSpecific,
+}
+
+/// The platform specific window settings of an application.
+#[cfg(not(target_os = "macos"))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, DefaultDefault)]
+pub struct PlatformSpecific;
+
+/// The platform specific window settings of an application.
+#[cfg(target_os = "macos")]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct PlatformSpecific {
+    /// Activation policy for the application.
+    pub activation_policy: ActivationPolicy,
+
+    /// Used to prevent the application from automatically activating when launched if
+    /// another application is already active.
+    ///
+    /// The default behavior is to ignore other applications and activate when launched.
+    pub activate_ignoring_other_apps: bool,
+}
+
+#[cfg(target_os = "macos")]
+impl Default for PlatformSpecific {
+    fn default() -> Self {
+        Self {
+            activation_policy: ActivationPolicy::default(),
+            activate_ignoring_other_apps: true,
+        }
+    }
+}
+
+/// Corresponds to `NSApplicationActivationPolicy`.
+#[cfg(target_os = "macos")]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum ActivationPolicy {
+    /// Corresponds to `NSApplicationActivationPolicyRegular`.
+    #[default]
+    Regular,
+    /// Corresponds to `NSApplicationActivationPolicyAccessory`.
+    Accessory,
+    /// Corresponds to `NSApplicationActivationPolicyProhibited`.
+    Prohibited,
 }
 
 impl From<core::Settings> for Settings {
@@ -21,6 +66,7 @@ impl From<core::Settings> for Settings {
         Self {
             id: settings.id,
             fonts: settings.fonts,
+            platform_specific: Default::default(),
         }
     }
 }
